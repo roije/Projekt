a0d8aa1eb9a4b76c8218e1e46ac0f20e878ece4b
roije <roijellingsgaard@gmail.com>
2015-11-13 12:33:26 +0100

    sdfsf

--- src/VikaTedf.java
+++ src/VikaTedf.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by roije on 11/11/2015.
  */
@@ -5,14 +7,24 @@ public class VikaTedf
 {
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
         System.out.println("Hey");
         System.out.println("Hadji");
 
         Hadji hadji = new Hadji();
         System.out.println("fdg");
         System.out.println("fdg");
+        System.out.print("Is Hadji in school? ");
+        String answer = input.next();
+        if (answer.equals("Yes"))
+        {
+            hadji.setInSchool(true);
+        }
+        else if(answer.equals("No"))
+        {
+            hadji.setInSchool(false);
+        }
 
-        hadji.setInSchool(false);
         if (hadji.isInSchool())
         {
             System.out.println("Hadji power");
