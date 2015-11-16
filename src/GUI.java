7d274c43f97bf7942293368ae33db5d0b375d435
christianhasselstrom <crellecrelle@gmail.com>
2015-11-16 11:19:55 +0100

    Tabs?

--- src/GUI.java
+++ src/GUI.java
@@ -1,12 +1,23 @@
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
  * Created by christianhasselstrom on 16/11/2015.
  */
 public class GUI extends Application {
+
+
+    public GUI()
+    {
+
+    }
 
     public static void main(String[] args)
     {
@@ -17,9 +28,16 @@ public class GUI extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         primaryStage.setTitle("VikarApp");
-        Group root = new Group();
+        GridPane root = new GridPane();
         Scene scene1 = new Scene(root, 800, 800);
 
+        TabPane tabPane = new TabPane();
+        Tab tab = new Tab();
+        tab.setText("new tab");
+        tab.setContent(new Rectangle(200,200, Color.LIGHTSTEELBLUE));
+        tabPane.getTabs().add(tab);
+
+        root.getChildren().addAll(tabPane);
         primaryStage.setScene(scene1);
         primaryStage.show();
 
