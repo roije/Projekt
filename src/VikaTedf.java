0203f51b368ad55f0d20a08f494fea03049520c3
roije <roijellingsgaard@gmail.com>
2015-11-16 10:22:51 +0100

    fdgfdg

--- src/VikaTedf.java
+++ src/VikaTedf.java
@@ -15,6 +15,7 @@ public class VikaTedf
         System.out.println("fdg");
         System.out.println("fdg");
         System.out.print("Is Hadji in school? ");
+        System.out.print("");
         String answer = input.next();
         if (answer.equals("Yes"))
         {
