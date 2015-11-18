9ca6c5a81e6e38505f37a81a51e6be9a921503ef
christianhasselstrom <crellecrelle@gmail.com>
2015-11-18 10:23:03 +0100

    GUI updates

--- src/GUI.java
+++ src/GUI.java
@@ -26,6 +26,8 @@ public class GUI extends Application {
     Label headLine;
     VBox topVBox;
     HBox buttonBox;
+
+
     public void GUI()
     {
         //BorderPane
@@ -68,7 +70,6 @@ public class GUI extends Application {
     {
         GUI();
 
-
         //Label
         headLine = new Label("Vicarius");
         Reflection r = new Reflection();
@@ -77,8 +78,6 @@ public class GUI extends Application {
         headLine.setTextFill(Color.GHOSTWHITE);
         headLine.setFont(Font.font("Verdana", FontWeight.BOLD, 60));
 
-
-
         //sellersBtn
         sellersBtn = new ToggleButton("Sellers");
         sellersBtn.setPrefHeight(20);
@@ -86,7 +85,10 @@ public class GUI extends Application {
         sellersBtn.setFont(Font.font("Verdana"));
         sellersBtn.setStyle("-fx-background-color: linear-gradient(#fafdfe, #a7d9f5)");
         buttonBox.getChildren().add(sellersBtn);
-
+        sellersBtn.setOnAction(e ->
+        {
+            root.setCenter(sellersTable());
+        });
 
         //buyersBtn
         buyersBtn = new ToggleButton("Buyers");
@@ -95,6 +97,10 @@ public class GUI extends Application {
         buyersBtn.setFont(Font.font("Verdana"));
         buyersBtn.setStyle("-fx-background-color: linear-gradient(#fafdfe, #a7d9f5)");
         buttonBox.getChildren().add(buyersBtn);
+        buyersBtn.setOnAction(e ->
+        {
+            root.setCenter(buyersTable());
+        });
 
         //matchesBtn
         matchesBtn = new ToggleButton("Matches");
@@ -103,6 +109,10 @@ public class GUI extends Application {
         matchesBtn.setFont(Font.font("Verdana"));
         matchesBtn.setStyle("-fx-background-color: linear-gradient(#fafdfe, #a7d9f5)");
         buttonBox.getChildren().add(matchesBtn);
+        matchesBtn.setOnAction(e ->
+        {
+            root.setCenter(matchesTable());
+        });
 
         //taskBtn
         tasksBtn = new ToggleButton("Tasks");
@@ -111,6 +121,10 @@ public class GUI extends Application {
         tasksBtn.setFont(Font.font("Verdana"));
         tasksBtn.setStyle("-fx-background-color: linear-gradient(#fafdfe, #a7d9f5)");
         buttonBox.getChildren().add(tasksBtn);
+        tasksBtn.setOnAction(e ->
+        {
+            root.setCenter(tasksTable());
+        });
 
         //myProfileBtn
         myProfileBtn = new ToggleButton("My Profile");
@@ -120,6 +134,10 @@ public class GUI extends Application {
         myProfileBtn.setToggleGroup(tGroup);
         myProfileBtn.setStyle("-fx-background-color: linear-gradient(#fafdfe, #a7d9f5)");
         buttonBox.getChildren().add(myProfileBtn);
+        myProfileBtn.setOnAction(e ->
+        {
+            root.setCenter(null);
+        });
 
         //signOutBtn
         signOutBtn = new ToggleButton("Sign Out");
@@ -127,10 +145,92 @@ public class GUI extends Application {
 
         topVBox.getChildren().addAll(headLine, buttonBox);
     }
+    public TableView sellersTable()
+    {
+        TableView sellersTable = new TableView();
 
+        sellersTable.setPrefWidth(400);
+        TableColumn name = new TableColumn("Name");
+        TableColumn age = new TableColumn("Age");
+        TableColumn location = new TableColumn("Location");
+        TableColumn qualifications = new TableColumn("Qualifications");
+        TableColumn rating = new TableColumn("Rating");
 
+        name.setPrefWidth(150);
+        age.setPrefWidth(150);
+        location.setPrefWidth(150);
+        qualifications.setPrefWidth(150);
+        rating.setPrefWidth(150);
 
+        sellersTable.getColumns().addAll(name, age, location, qualifications, rating);
 
+        return sellersTable;
+    }
 
+    public TableView buyersTable()
+    {
+        TableView buyersTable = new TableView();
 
+        buyersTable.setPrefWidth(400);
+        TableColumn buyerDescription = new TableColumn("Buyer description");
+        TableColumn location = new TableColumn("Location");
+        TableColumn qualifications = new TableColumn("Qualifications");
+        TableColumn rating = new TableColumn("Rating");
+
+        buyerDescription.setPrefWidth(200);
+        location.setPrefWidth(150);
+        qualifications.setPrefWidth(150);
+        rating.setPrefWidth(50);
+
+        buyersTable.getColumns().addAll(buyerDescription, location, qualifications, rating);
+
+        return buyersTable;
+    }
+
+    public TableView matchesTable()
+    {
+        TableView matchesTable = new TableView();
+
+        matchesTable.setPrefWidth(400);
+        TableColumn jobDescription = new TableColumn("Job description");
+        TableColumn buyerDescription = new TableColumn("Buyer description");
+        TableColumn location = new TableColumn("Location");
+        TableColumn qualifications = new TableColumn("Qualification(s)");
+        TableColumn rating = new TableColumn("Rating");
+        TableColumn salary = new TableColumn("Salary");
+
+        jobDescription.setPrefWidth(300);
+        buyerDescription.setPrefWidth(200);
+        location.setPrefWidth(150);
+        qualifications.setPrefWidth(150);
+        rating.setPrefWidth(50);
+        salary.setPrefWidth(80);
+
+        matchesTable.getColumns().addAll(jobDescription, buyerDescription, location, qualifications, rating, salary);
+
+        return matchesTable;
+    }
+
+    public TableView tasksTable() {
+        TableView tasksTable = new TableView();
+
+        tasksTable.setPrefWidth(400);
+        TableColumn jobDescription = new TableColumn("Job description");
+        TableColumn buyerDescription = new TableColumn("Buyer description");
+        TableColumn location = new TableColumn("Location");
+        TableColumn qualifications = new TableColumn("Qualification(s)");
+        TableColumn rating = new TableColumn("Rating");
+        TableColumn salary = new TableColumn("Salary");
+
+        jobDescription.setPrefWidth(300);
+        buyerDescription.setPrefWidth(200);
+        location.setPrefWidth(150);
+        qualifications.setPrefWidth(150);
+        rating.setPrefWidth(50);
+        salary.setPrefWidth(80);
+
+        tasksTable.getColumns().addAll(jobDescription, buyerDescription, location, qualifications, rating, salary);
+
+        return tasksTable;
+    }
 }
